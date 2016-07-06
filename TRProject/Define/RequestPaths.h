dd235f4bb21826d7db319a8c4434bdfffec92f9e
mahuakang <git@github.com:mahuakang/nmb.git>
2016-07-06 17:29:45 +0800

    first put up

---
+++ TRProject/Define/RequestPaths.h
@@ -0,0 +1,22 @@
+//
+//  RequestPaths.h
+//  BestGroup
+//
+//  Created by jiyingxin on 16/6/8.
+//  Copyright © 2016年 Tedu. All rights reserved.
+//
+
+#ifndef RequestPaths_h
+#define RequestPaths_h
+
+#define kBasePath @""
+
+
+
+#endif /* RequestPaths_h */
+
+
+
+
+
+
